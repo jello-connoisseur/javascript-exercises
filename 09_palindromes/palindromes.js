8df382a72ca00ff061ea1f775435fdecdc48191c
Jewon Hwang <jewon.hwang@gmail.com>
2021-11-20 16:11:38 -0500

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+    const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    let result = str.replace(regex, '');
+    result = result.replace(/\s/g, '');
+    result = result.toLowerCase();
+
+
+
+    for(let i = 0 ; i < (result.length-1)/2; i++){
+        if (result[i] === result[result.length-1-i]){
+            return true;
+        }
+        else return false;
+    }
 
 };
 
