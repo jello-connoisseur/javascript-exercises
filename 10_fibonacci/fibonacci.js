70f64ab8b0d52a997d93e109c86bf8e6a43b902f
Jewon Hwang <jewon.hwang@gmail.com>
2021-11-21 13:44:28 -0500

    solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+
+    if (num < 0){
+        return "OOPS";
+    }
+    let fib = [];
+    fib[0] = 1;
+    fib[1] = 1;
+
+    for (i = 3; i <= num; i++){
+        let newFib = fib[i-2] + fib[i-3];
+        fib.push(newFib);
+    }
+
+    return fib[num-1];
 
 };
 
