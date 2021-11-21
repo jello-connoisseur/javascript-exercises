468bf4b90ce7cbc4d078caef48901ec4fe829bc3
Jewon Hwang <jewon.hwang@gmail.com>
2021-11-21 14:01:21 -0500

    solved

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let bookArray = [];
+    books.map(function(book){
+        bookArray.push(book.title);
+    })
+    return bookArray;
 };
 
 // Do not edit below this line
