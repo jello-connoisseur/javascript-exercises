bac8f48809678de1261bec14988e86de18bf9a69
Jewon Hwang <jewon.hwang@gmail.com>
2021-11-21 19:34:19 -0500

    solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+
+    //if still alive, calculated their age
+    const currentYear = (new Date()).getFullYear();
+    for (i = 0; i < people.length; i++){
+        if (!people[i].hasOwnProperty('yearOfDeath')){
+            people[i].yearOfDeath = currentYear;
+        }
+    }
+    
+    //using sort
+    const sortedPeople = people.sort(function(person1, person2){
+        const lastGuy = person1.yearOfDeath - person1.yearOfBirth;
+        const nextGuy = person2.yearOfDeath - person2.yearOfBirth;
+
+        if(lastGuy > nextGuy){
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+
+    return sortedPeople[0];
+
 
 };
 
